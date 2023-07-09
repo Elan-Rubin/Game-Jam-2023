d80865cbd91b4d4869c893a5d118574581971a46
elan_rubin <elandanielrubin@gmail.com>
2023-07-08 23:09:28 -0600

    added more stuff

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Core.Environments;
 using UnityEngine;
 
 public class Environment : MonoBehaviour
@@ -10,17 +11,36 @@ public class Environment : MonoBehaviour
     [SerializeField] float playerPositionWeighting;
 
     private GameObject player;
+    private Vector3 movement;
+    public Vector3 Movement { get { return movement; } }
+
+    private Vector3 totalOffset = Vector3.zero;
+    public Vector3 TotalOffset { get {  return totalOffset; } }
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
     }
+    #region singleton
+    private static Environment _instance;
 
+    public static Environment Instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+        else
+            _instance = this;
+    }
+    #endregion
     private void Update()
     {
         //the player should descend faster when closer to the bottom of the screen
         float playerPositionMultiplier = (7 - player.transform.position.y) * playerPositionWeighting;
         //move this gameobject upwards each frame
-        transform.position += Vector3.up * speedmultiplier * playerPositionMultiplier * Time.deltaTime;
+        movement = Vector3.up * speedmultiplier * playerPositionMultiplier * Time.deltaTime;
+        transform.position += movement;
+        totalOffset += movement;
+        //PlayerMovement.Instance.UpdateString();
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -27,13 +29,58 @@ public class PlayerMovement : MonoBehaviour
     // Vector2 representing the movement input
     private Vector2 movement;
     private Vector2 position;
-    [HideInInspector] public Vector2 Position { get { return position; } }
+    private int counter;
+    [SerializeField] private LineRenderer lineRenderer;
 
+    [HideInInspector] public Vector2 Position { get { return position; } }
+    private void Start()
+    {
+        //lineRenderer = transform.GetChild(0).GetComponent<LineRenderer>();
+    }
     private void FixedUpdate()
     {
         Movement();
         position = transform.position;
     }
+    private void Update()
+    {
+        //var positions = new Vector3[lineRenderer.positionCount];
+        //lineRenderer.GetPositions(positions);
+        //var newPositions = new Vector3[positions.Length + 1];
+        //for (int i = 0; i < positions.Length; i++)
+        //{
+        //    newPositions[i] = positions[i];
+        //}
+        //newPositions[newPositions.Length - 1] = transform.position;
+        ////positions[positions.Length-1] = transform.position;
+        //lineRenderer.SetPositions(newPositions);
+
+        //for (int i = 0; i < lineRenderer.positionCount; i++)
+        //{
+        //    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i));
+        //}
+
+        counter++;
+        if (counter % 10 == 0)
+        {
+            lineRenderer.positionCount++;
+            lineRenderer.SetPosition(lineRenderer.positionCount -1, transform.GetChild(0).position - Environment.Instance.TotalOffset);
+            Debug.Log(transform.GetChild(0).position);
+        }
+        
+    }
+    //public void UpdateString()
+    //{
+    //    Vector3[] newList = new Vector3[lineRenderer.positionCount];
+    //    lineRenderer.GetPositions(newList);
+    //    var offset = Environment.Instance.Movement;
+    //    var newestList = new List<Vector3>();
+    //    foreach (var x in newList)
+    //    {
+    //        newestList.Add(x + offset);
+    //    }
+    //    lineRenderer.SetPositions(newestList.ToArray());
+    //}
 
     private void Movement()
     {
