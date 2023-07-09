0a35b3a3e6a4e84f790b0cbef19e363ce2156be3
Leo Norton <leoznorton@gmail.com>
2023-07-08 23:25:08 -0600

    Redid the player movement script and made it screen clamped

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,12 +22,25 @@ public class PlayerMovement : MonoBehaviour
     // Speed of the game object during normal movement
     public float movementSpeed = 10.0f;
 
+    private Camera mainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
     // Rigidbody2D component of the game object
     private Rigidbody2D rb;
     // Vector2 representing the movement input
     private Vector2 movement;
     private Vector2 position;
     [HideInInspector] public Vector2 Position { get { return position; } }
+
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
 
     private void FixedUpdate()
     {
@@ -40,7 +53,12 @@ public class PlayerMovement : MonoBehaviour
         // Get the movement input as a Vector2 using Input.GetAxis
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-        // Move the game object in the direction of the movement vector at the movement speed
-        rb.velocity = movement * movementSpeed;
+
+        Vector2 newPos = transform.position;
+        newPos += movement * Time.deltaTime * movementSpeed;
+        newPos.x = Mathf.Clamp(newPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        newPos.y = Mathf.Clamp(newPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+
+        transform.position = newPos;
     }
 }
